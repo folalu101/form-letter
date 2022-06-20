daed771a89b543c4e0664b1904b58505b6c5836c
A.Adefolalu B <adefolalucode101@gmail.com>
2022-06-20 20:30:21 +0100

    write a function to submit all id from the form to print infortion

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("#form-letter").submit(function(event) {
+        const name1 = $("#name-1").val();
+        const name2 = $("#name-2").val();
+        const name3 = $("#name-3").val();
+        const number1 = $("#number-1").val();
+        const email1 = $("#email-1").val();
+        const address1 = $("#address-1").val();
+        const address2 = $("#address-2").val();
+        const address3 = $("#address-3").val();
+        const number2 = $("#number-2").val();
+        const name4 = $("#name-4").val();
+        const address4 = $("#address-4").val();
+        const number3 = $("#number-3").val();
+        const relative1 = $("#relative-1").val();
+
+    })
+
+})
